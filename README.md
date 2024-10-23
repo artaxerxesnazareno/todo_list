a5f262b2338d67d95d4c2197ffa3d6cb0dc2def8
= <artaxerxesnazare@hotmail.com>
2024-10-23 14:38:18 +0100

    feat: Adicionar funcionalidade de lista de tarefas
    Adiciona uma tela de lista de tarefas com as seguintes funcionalidades:
    * Adicionar novas tarefas
    * Marcar tarefas como concluídas
    * Remover tarefas
    * Ordenar tarefas por ordem de conclusão
    * Persistir tarefas no armazenamento local

--- README.md
+++ README.md
@@ -1,16 +1,58 @@
-# to_do_list
+# TO-DO List App
 
-A new Flutter project.
+## Descrição
 
-## Getting Started
+Este é um aplicativo simples de lista de tarefas desenvolvido em Flutter. Ele permite aos usuários gerenciar suas tarefas diárias de forma eficiente e intuitiva.
 
-This project is a starting point for a Flutter application.
+## Funcionalidades
 
-A few resources to get you started if this is your first Flutter project:
+- Adicionar novas tarefas
+- Marcar tarefas como concluídas
+- Remover tarefas deslizando para o lado
+- Desfazer a remoção de tarefas
+- Ordenar tarefas automaticamente (concluídas e não concluídas)
+- Persistência de dados (as tarefas são salvas localmente)
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+## Capturas de Tela
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+![alt text](image.png)
+
+
+## Instalação
+
+1. Certifique-se de ter o Flutter instalado em seu sistema. Para instruções de instalação, visite [flutter.dev](https://flutter.dev/docs/get-started/install).
+
+2. Clone este repositório:
+   ```
+   git clone https://github.com/seu-usuario/to-do-list-app.git
+   ```
+
+3. Navegue até o diretório do projeto e instale as dependências:
+   ```
+   cd to-do-list-app
+   flutter pub get
+   ```
+
+4. Execute o aplicativo:
+   ```
+   flutter run
+   ```
+
+## Uso
+
+![alt text](Screen_recording_20241023_143216.gif)
+
+1. Para adicionar uma nova tarefa, digite o texto no campo de entrada na parte superior da tela e pressione o botão "ADD".
+2. Para marcar uma tarefa como concluída, toque no checkbox ao lado da tarefa.
+3. Para remover uma tarefa, deslize-a para a direita.
+4. Se remover uma tarefa por engano, use a opção "Desfazer" que aparece na parte inferior da tela.
+
+## Tecnologias Utilizadas
+
+- Flutter
+- Dart
+- path_provider (para persistência de dados local)
+
+
+![img.png](img.png)
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,192 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class HomeScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class _HomeScreenState extends State<HomeScreen> {
+  List _todoList = [];
+  TextEditingController _taskController = TextEditingController();
+  Map<String, dynamic> _lastRemoved = {};
+  int _lastRemovedPos = 0;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void initState() {
+    super.initState();
+    _readData().then((data) {
+      setState(() {
+        _todoList = jsonDecode(data);
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title:
+              const Text('TO-DO List', style: TextStyle(color: Colors.white)),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(17, 1, 7, 1),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _taskController,
+                      decoration: const InputDecoration(
+                          hintText: 'Nova Tarefa',
+                          labelText: 'Nova Tarefa',
+                          labelStyle: TextStyle(color: Colors.blueAccent)),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  ElevatedButton(
+                    onPressed: _addTask,
+                    child: const Text('ADD',
+                        style: TextStyle(color: Colors.white)),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueAccent,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Expanded(
+                child: RefreshIndicator(
+                  onRefresh: _refresh,
+                  child: ListView.builder(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    itemCount: _todoList.length,
+                    itemBuilder: buildItem,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: const Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_todoList[index]["title"]),
+        value: _todoList[index]["ok"],
+        onChanged: (value) {
+          setState(() {
+            _todoList[index]["ok"] = value;
+            // _saveFile(jsonEncode(_todoList));
+          });
+        },
+        activeColor: Colors.blueAccent,
+        checkColor: Colors.white,
+        secondary: CircleAvatar(
+          child: Icon(_todoList[index]["ok"] ? Icons.check : Icons.info_outline,
+              color: Colors.white),
+          backgroundColor: Colors.blueAccent,
+        ),
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = _todoList[index];
+          _lastRemovedPos = index;
+          _todoList.removeAt(index);
+          _saveData(jsonEncode(_todoList));
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            duration: Duration(seconds: 2),
+            content: Text('Tarefa \"${_lastRemoved["title"]}\"Removida'),
+            action: SnackBarAction(
+              label: 'Desfazer',
+              onPressed: () {
+                setState(() {
+                  _todoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData(jsonEncode(_todoList));
+                });
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _addTask() {
+    if (_taskController.text.isNotEmpty) {
+      Map<String, dynamic> newTask = {
+        "title": _taskController.text,
+        "ok": false
+      };
+      setState(() {
+        _todoList.add(newTask);
+        _taskController.clear();
+      });
+      _saveData(jsonEncode(_todoList));
+    }
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File('${directory.path}/data.json');
+  }
+
+  Future<File> _saveData(String text) async {
+    String data = jsonEncode(_todoList);
+    final file = await _getFile();
+    return file.writeAsString(text);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return 'Error: $e';
+    }
+  }
+
+  Future<void> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _todoList.sort(
+        (a, b) {
+          if (a["ok"] && !b["ok"])
+            return 1;
+          else if (!a["ok"] && b["ok"])
+            return -1;
+          else
+            return 0;
+        },
+      );
+      _saveData(jsonEncode(_todoList));
+    });
   }
 }
